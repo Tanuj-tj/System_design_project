1659d4a104236a5c4f3e058f27d52741cd21f7ec
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2024-01-03 09:14:17 +0530

    Delete DesignPatterns/composit/file.py

--- DesignPatterns/composit/file.py
+++
@@ -1,9 +0,0 @@
-from composit.file_system import FileSystem
-
-class File(FileSystem):
-
-    def __init__(self, name: str):
-        self.file_name = name
-
-    def ls(self) -> None:
-        print(f"File name {self.file_name}")
